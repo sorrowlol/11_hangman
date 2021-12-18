08b4b2103d541ac86d3a055531b0877bbcd999f6
sorrowlol <sorrowlol@outlook.com>
2021-12-18 16:30:59 +0100

    rubocop fix

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'json'
 
 class Hangman
-
   def initialize
     @dictionary = File.open('5desk.txt')
     @secret_word = ''
@@ -14,95 +15,92 @@ class Hangman
     @word.delete(' ')
     @secret_word = @word[rand(@word.length)].split('')
   end
-  
+
   def load_game
     puts "Welcome to Hangman! You loaded from last game saved!  You have #{6 - @incorrect_letters.length} lives left! Don't die!"
     puts @display.join(' ')
     puts "Your word has #{@secret_word.length} letters!"
     player_picks
   end
- 
-  #secret word
+
+  # secret word
   def start_game
     picks_word
-    @display = Array.new(@secret_word.length, "_")
-    puts "Welcome to Hangman! You have 6 guesses before you hang... make them count!"
+    @display = Array.new(@secret_word.length, '_')
+    puts 'Welcome to Hangman! You have 6 guesses before you hang... make them count!'
     puts @display.join(' ')
     puts "Your word has #{@secret_word.length} letters!"
     player_picks
   end
 
-  #choose a letter function
+  # choose a letter function
   def player_picks
     save if @turn >= 1
-    load if @turn == 0
-    puts "Choose any letter (case insensitive)"
+    load if @turn.zero?
+    puts 'Choose any letter (case insensitive)'
     @letter_pick = gets.chomp.downcase
-   
-  
-  #if letter already picked
+
+    # if letter already picked
     if @display.include?(@letter_pick)
-      puts "You already used this letter! Pick another one"
+      puts 'You already used this letter! Pick another one'
       player_picks
-  #if letter included in word
+    # if letter included in word
     elsif @secret_word.include?(@letter_pick)
       @secret_word.each_with_index do |i, idx|
-        if i == @letter_pick
-          @display[idx] = @letter_pick
-        end
+        @display[idx] = @letter_pick if i == @letter_pick
       end
       puts "nice! you got letter #{@letter_pick}"
 
-  #if letter not included in word and pushes letter to incorrect array
+    # if letter not included in word and pushes letter to incorrect array
     else
       @incorrect_letters.push(@letter_pick)
       puts "Lives left #{6 - @incorrect_letters.length} out of 6"
-  #if 6 tries game over 
+      # if 6 tries game over
       if @incorrect_letters.length == 6
         puts @display.join(' ')
         puts "Incorrect letters chosen: #{@incorrect_letters}"
-        puts "you lose!"
+        puts 'you lose!'
         puts "The word was #{@secret_word.join('')}"
         return
       end
     end
-   
+
     @turn += 1
     puts @display.join(' ')
 
-  #runs win or loops again back to choose a letter -> player_picks 
-    
+    # runs win or loops again back to choose a letter -> player_picks
+
     if win
       puts 'yay you win!'
       puts "the word was #{@secret_word.join('')}"
     else
-     puts "Incorrect letters chosen: #{@incorrect_letters}"
-     player_picks
+      puts "Incorrect letters chosen: #{@incorrect_letters}"
+      player_picks
     end
   end
 
   def win
     @display.join('') == @secret_word.join('')
   end
-  
+
   def save
-    puts "Would you like to save the game and load later? (y/n)"
+    puts 'Would you like to save the game and load later? (y/n)'
     player_sav = gets.chomp
     if player_sav == 'y'
       saved_hash = {
         s_secret_word: @secret_word,
         s_incorrect_letters: @incorrect_letters,
         s_display: @display,
-        s_turn: @turn,        
+        s_turn: @turn
       }
-      File.open("saved_game.json", 'a') do |f|
+      File.open('saved_game.json', 'a') do |f|
         f.puts(saved_hash.to_json)
       end
     end
   end
 
   def load
-    puts "Would you like to load last saved game?"
+    puts 'Would you like to load last saved game?'
     player_load = gets.chomp
     if player_load == 'y'
       load_file = File.read('saved_game.json')
